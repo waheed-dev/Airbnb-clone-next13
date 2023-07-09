80595438a086ed2f1d820b1024b670c430948036
Waheed <a.waheed3738@gmail.com>
2023-07-09 10:53:50 +0500

    fix error

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,30 +1,45 @@
-import {User} from "@prisma/client";
+import { User } from "@prisma/client";
 import React from "react";
 import useCountries from "@/app/hooks/useCountries";
 import Heading from "@/app/Heading";
-import Image from 'next/image'
+import Image from "next/image";
 import HeartButton from "@/app/components/HeartButton";
+
 interface ListingHeadProps {
-    title  : string
-    locationValue : string
-    imageSrc : string
-    id : string
-    currentUser : User | null
+  title: string;
+  locationValue: string;
+  imageSrc: string;
+  id: string;
+  currentUser: User | null;
 }
 
-const ListingHead : React.FC<ListingHeadProps> = ({currentUser,id,title,imageSrc,locationValue}) => {
-   const {getByValue} = useCountries()
-    const location = getByValue(locationValue)
-    return (
-        <>
-        <Heading title={title} subtitle={`${location?.region},${location?.label}`}/>
-            <div className={'w-full h-[60vh] overflow-hidden rounded-xl relative'}>
-            <Image alt={title} src={imageSrc} fill className={'object-cover w-full'}/>
-                <div className={'absolute top-5 right-5'}>
-                    <HeartButton listingId={id} currentUser={currentUser}/>
-                </div>
-            </div>
-        </>
-    )
-}
+const ListingHead: React.FC<ListingHeadProps> = ({
+  currentUser,
+  id,
+  title,
+  imageSrc,
+  locationValue,
+}) => {
+  const { getByValue } = useCountries();
+  const location = getByValue(locationValue);
+  return (
+    <>
+      <Heading
+        title={title}
+        subtitle={`${location?.region},${location?.label}`}
+      />
+      <div className={"w-full h-[60vh] overflow-hidden rounded-xl relative"}>
+        <Image
+          alt={title}
+          src={imageSrc}
+          fill
+          className={"object-cover w-full"}
+        />
+        <div className={"absolute top-5 right-5"}>
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
+    </>
+  );
+};
 export default ListingHead
